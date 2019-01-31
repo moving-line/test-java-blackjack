cdf40656273a746c54951ce2c63fbf215b9412e6
Jinsan Noh <zingoworks@gmail.com>
2019-01-31 16:38:51 +0900

    Develop1 PR (#30)
    * 첫번째날 첫번째커밋
    
    * 첫번째날 두번째커밋
    
    * Feature/login (#28)
    
    * 핀이 로컬 로그인 브랜치 생성
    
    * 핀이 한줄을 추가하다
    
    * Feature/user (#29)
    
    * 징고가 집에서 유저클래스만들었어
    
    * 유저기능 구현 쫌 해왔어여
    
    * 두번째날 페어로 짠 코드

--- src/main/java/com/codesquad/blackjack/BlackjackApplication.java
+++ src/main/java/com/codesquad/blackjack/BlackjackApplication.java
@@ -10,5 +10,9 @@ public class BlackjackApplication {
 		SpringApplication.run(BlackjackApplication.class, args);
 	}
 
+	//test1 만듬
+	//또만들어 구현막함 같이 쌰바샤
+
+	//두번째날에 유저,로그인은 각자 만들어왔고 그거 수정하고 테스트도 만들고 페어로해염
 }
 
---
+++ src/main/java/com/codesquad/blackjack/User.java
@@ -0,0 +1,6 @@
+package com.codesquad.blackjack;
+
+public class User {
+    //징고가 집에서 유저를 만들어 왔어요
+    //징고가 집에서 유저만들고 기능구현했어
+}
